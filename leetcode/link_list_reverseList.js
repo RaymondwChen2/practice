b9a514d824d56745b0a4e5a05d6064a99fc7810b
RaymondwChen2 <raymondwchen2@gmail.com>
2022-06-20 17:35:44 -0700

    complete reverse list

---
+++ leetcode/link_list_reverseList.js
@@ -0,0 +1,29 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null
+    }
+}
+
+let a = new Node('a');
+let b = new Node('b');
+let c = new Node('c');
+
+a.next = b;
+b.next = c;
+
+let reverseList = (head) => {
+    let curr = head;
+    let prev = null;
+
+    while (curr !== null) {
+        let next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next;
+    }
+
+    return prev;
+}
+
+console.log(reverseList(a));
